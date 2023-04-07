ee4786829c17e97e39fb87ebd71ae42171f4b829
Omniscient <17525998+omnisci3nce@users.noreply.github.com>
2023-04-07 17:32:03 +1000

    add wezterm config file

---
+++ home/dot_config/wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require('wezterm')
+
+local config = {}
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.font_size = 11.0
+
+config.window_padding = {
+  left = 2,
+  right = 2,
+  top = 0,
+  bottom = 0,
+}
+
+--  font = wezterm.font('MesloLGM Nerd Font Mono'),
+
+return config
