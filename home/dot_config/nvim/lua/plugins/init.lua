ea29212f26f15a11d6665af23dec679b8ff0eeb5
Joshua <josh@ai-nc.com>
2023-04-17 16:57:46 +1000

    change to nvim-tree

--- home/dot_config/nvim/lua/plugins/init.lua
+++ home/dot_config/nvim/lua/plugins/init.lua
@@ -13,12 +13,13 @@ return {
   },
   'tpope/vim-fugitive',
   {
-    'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v2.x',
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      --"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
   },
+  config = function()
+    require("nvim-tree").setup {}
+  end,
+}
 }
