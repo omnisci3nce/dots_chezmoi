f38b66a2b6bfd5cfa90600392ac797a14b80a566
Joshua <josh@ai-nc.com>
2023-04-17 16:34:34 +1000

    update nvim plugins. configure Berkeley Mono

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -5,17 +5,26 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.font_size = 11.0
-
 config.window_padding = {
   left = 2,
   right = 2,
-  top = 0,
+  top = 2,
   bottom = 0,
 }
 
---  font = wezterm.font('MesloLGM Nerd Font Mono'),
 
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Fonts
+config.font = wezterm.font('Berkeley Mono')
+config.font_size = 10.5
+config.line_height = 1.1
+config.cell_width = 0.9
+
+-- Colours 
 config.color_scheme = "Tomorrow Night Bright"
+
+config.audible_bell = "Disabled"
+config.default_prog = { '/bin/bash' }
 
 return config
